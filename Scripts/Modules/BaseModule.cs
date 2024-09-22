6552053743a002486cf9982ba3abcd081f57a253
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 04:13:52 +1000

    misc

---
+++ Scripts/Modules/BaseModule.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+
+namespace GameDevInc;
+
+public enum EModuleJobType
+{
+    JOB_Programmer,
+    JOB_2DArtist,
+    JOB_3DArtist,
+    JOB_SoundEngineer,
+    JOB_Marketing,
+    JOB_Design,
+    JOB_UIUX
+}
+
+public enum EGameSize
+{
+    GAME_Indie,
+    GAME_III,
+    GAME_AAA
+}
+
+public class BaseModule
+{
+    public string ModuleName { get; private set;  }
+    public EModuleJobType ModuleJobType { get; private set; }
+
+    // == Number Sets == //
+    private int m_MinIndiePoints;
+    private int m_MaxIndiePoints;
+
+    private int m_MinIIIPoints;
+    private int m_MaxIIIPoints;
+
+    private int m_MinAAAPoints;
+    private int m_MaxAAAPoints;
+
+    public int GetRequiredProgrammingPoints(EGameSize gameSize, float pointModifier = 1f)
+    {
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();  
+        switch(gameSize)
+        {
+            case EGameSize.GAME_Indie:
+                return Mathf.FloorToInt(rand.RandiRange(m_MinIndiePoints, m_MaxIndiePoints) * pointModifier);
+            case EGameSize.GAME_III:
+                return Mathf.FloorToInt(rand.RandiRange(m_MinIIIPoints, m_MaxIIIPoints) * pointModifier);
+            case EGameSize.GAME_AAA:
+                return Mathf.FloorToInt(rand.RandfRange(m_MinAAAPoints, m_MaxAAAPoints) * pointModifier);
+            default: return 0;
+        }
+    }
+    
+}
