63718f6c28b975f8a0241826a3a34600ab024d5b
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 16:24:03 +1000

    Created placement

---
+++ Scripts/BuildingSystem/PlacementController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Godot;
+
+namespace GameDevInc.BuildingSystem;
+
+public partial class PlacementController : Node3D
+{
+    public bool IsPlacing = true;
+
+    public Node3D PlacingObject;                        // Object that we are placing
+    private GridSystem m_GridSystem;
+
+    private Camera3D? m_Camera;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        m_Camera = GetNode<Camera3D>("/root/SceneRoot/Camera3D");
+        if (m_Camera == null)
+            GD.PushWarning("PlacementController::_Ready -> Failed to get reference to the camera 3D");
+
+        m_GridSystem = (GridSystem)GetTree().GetFirstNodeInGroup("BuildingSystem");
+        if (m_GridSystem == null)
+            GD.PushWarning("PlacementController::_Ready -> Failed to find reference to the grid system");
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if (!IsPlacing || m_Camera == null || PlacingObject == null)
+            return;
+
+        var mousePos = GetViewport().GetMousePosition();
+        var rayOrigin = m_Camera.ProjectRayOrigin(mousePos);
+        var rayDirection = m_Camera.ProjectRayNormal(mousePos);
+        var rayLength = 1000f;
+        var rayEnd = rayOrigin + rayDirection * rayLength;
+
+        var space = GetWorld3D().DirectSpaceState;
+
+        var rayParams = new PhysicsRayQueryParameters3D
+        {
+            From = rayOrigin,
+            To = rayEnd,
+        };
+
+        var result = space.IntersectRay(rayParams);
+
+        if(result.Count > 0)
+        {
+            var hitPos = (Vector3)result["position"];
+            var gridPoint = new Vector2(hitPos.X, hitPos.Z);
+
+            if(m_GridSystem != null)
+            {
+                var cell = m_GridSystem.GetCell(gridPoint);
+                if (cell != null)
+                    PlacingObject.GlobalPosition = new Vector3(cell.GridCellPosition.X, m_GridSystem.Position.Y, cell.GridCellPosition.Y);
+            }
+        }
+    }
+}
