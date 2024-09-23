e9331bd6a44ce6ecf8f93691a316c58bc52bf0dc
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 14:57:14 +1000

    Base Genre setup

---
+++ Scripts/Game/Genre.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Godot;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GameDevInc;
+
+public class Genre
+{
+    private const string GenreResourcePath = "res://Data/Genre.json";
+
+    public string GenreName { get; protected set; }
+
+    private static List<Genre> LoadedGenres = new List<Genre>();
+
+    public Genre(string genreName)
+    {
+        GenreName = genreName;
+    }
+
+    public static void LoadGenres()
+    {
+        if(FileAccess.FileExists(GenreResourcePath))
+        {
+            var file = FileAccess.Open(GenreResourcePath, FileAccess.ModeFlags.Read);
+            if(file != null && file.IsOpen())
+            {
+                var dataText = file.GetAsText();
+                var tokens = JArray.Parse(dataText);
+
+                foreach(var token in tokens)
+                {
+                    var genreData = JsonConvert.DeserializeObject<GenreJson>(token.ToString());
+                    LoadedGenres.Add(new Genre(genreData.Name));
+                }
+            }
+        }
+    }
+}
+
+public class GenreJson
+{
+    [JsonProperty]
+    public string Name;
+}
