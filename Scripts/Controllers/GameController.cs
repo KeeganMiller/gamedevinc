52b48c6708464291ca6e73b6ba5765c8286e8b82
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 12:20:06 +1000

    misc

---
+++ Scripts/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GameDevInc;
+
+public partial class GameController : Node3D
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        BaseModule.LoadModules();
+        var modules = BaseModule.GetAllModules();
+        foreach (var m in modules)
+            GD.Print(m.ModuleName);
+    }
+}
