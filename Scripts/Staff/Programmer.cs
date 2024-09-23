c749713165a86d3581c017d8056d6bb85e49da50
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 13:31:48 +1000

    Added generating of stats

--- Scripts/Staff/Programmer.cs
+++ Scripts/Staff/Programmer.cs
@@ -5,14 +5,45 @@ namespace GameDevInc;
 
 public class Programmer : StaffMember
 {
-    public float Proficiency { get; private set; }
-    public float Mathematics { get; private set; }
-    public float Debugging { get; private set; }
-    public float GraphicsProgramming { get; private set; }
-    public float ArtificalIntelligence { get; private set; }
-    public float Networking { get; private set; }
+    public float Proficiency { get; protected set; }
+    public float Mathematics { get; protected set; }
+    public float Debugging { get; protected set; }
+    public float GraphicsProgramming { get; protected set; }
+    public float ArtificalIntelligence { get; protected set; }
+    public float Networking { get; protected set; }
 
     public Programmer(string name, EModuleJobType jobType) : base(name, jobType)
     {
+    }
+
+    protected override void GenerateStats()
+    {
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();
+        for(var i = 0; i < Level; ++i)
+        {
+            var stat = rand.RandiRange(0, 5);
+            switch(stat)
+            {
+                case 0:
+                    Proficiency += 1;
+                    break;
+                case 1:
+                    Mathematics += 1;
+                    break;
+                case 2:
+                    Debugging += 1;
+                    break;
+                case 3:
+                    GraphicsProgramming += 1;
+                    break;
+                case 4:
+                    ArtificalIntelligence += 1;
+                    break;
+                case 5:
+                    Networking += 1; 
+                    break;
+            }
+        }
     }
 }
