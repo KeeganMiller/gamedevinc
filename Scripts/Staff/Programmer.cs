716c026491882c9bc70db21b960516b125beb7bf
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 13:01:34 +1000

    Added properties

---
+++ Scripts/Staff/Programmer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GameDevInc;
+
+public class Programmer : StaffMember
+{
+    public float Proficiency { get; private set; }
+    public float Mathematics { get; private set; }
+    public float Debugging { get; private set; }
+    public float GraphicsProgramming { get; private set; }
+    public float ArtificalIntelligence { get; private set; }
+    public float Networking { get; private set; }
+
+    public Programmer(string name, EModuleJobType jobType) : base(name, jobType)
+    {
+    }
+}
