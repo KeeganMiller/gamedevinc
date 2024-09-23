2f8a0635608efc3c21f68f50c3b42ab98cfcf97c
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 14:56:59 +1000

    Comments

--- Scripts/Company/Company.cs
+++ Scripts/Company/Company.cs
@@ -18,12 +18,20 @@ public class Company
     private List<Company> m_OwnedStudios = new List<Company>();                     // List of children studios
     // TODO: Add List of games
 
+    /// <summary>
+    /// Adds a new game studio to this as owned
+    /// </summary>
+    /// <param name="studio">Studio to set as owned</param>
     public void AddOwnedStudio(Company studio)
     {
         m_OwnedStudios.Add(studio);
         studio.ParentCompany = this;
     }
 
+    /// <summary>
+    /// Removes a studio from this company
+    /// </summary>
+    /// <param name="studio">Studio to remove</param>
     public void RemoveOwnedStudio(Company studio)
     {
         if(m_OwnedStudios.Contains(studio))
@@ -33,6 +41,10 @@ public class Company
         }
     }
 
+    /// <summary>
+    /// Called each frame
+    /// </summary>
+    /// <param name="deltaTime"></param>
     public virtual void _Update(float deltaTime)
     {
         foreach(var staff in StaffMembers)
