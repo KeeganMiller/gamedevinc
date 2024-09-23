929566a7d0b1aa59933f021eaa005f9207111832
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-23 13:55:10 +1000

    Created the timer class

---
+++ Scripts/Controllers/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System;
+using Godot;
+
+namespace GameDevInc;
+
+public class Timer
+{
+    public float TimerLength;
+    private float m_CurrentTime;
+    public Action TimerCompleteAction;
+    public bool Loop;
+    public bool IsActive;
+
+    public Timer(float timerLength, Action timerCompleteAction, bool loop = false, bool isActive = true)
+    {
+        TimerLength = timerLength;
+        TimerCompleteAction = timerCompleteAction;
+        Loop = loop;
+        IsActive = isActive;
+    }
+
+    public void Pause(bool reset = true)
+    {
+        IsActive = false;
+        m_CurrentTime = reset ? 0 : m_CurrentTime;
+    }
+
+    public void Start(bool reset = true)
+    {
+        IsActive = true;
+        m_CurrentTime = reset ? 0 : m_CurrentTime;
+    }
+
+    public void _Update(float deltaTime)
+    {
+        if(!IsActive)
+            return;
+
+        m_CurrentTime += 1 * deltaTime;
+        if (m_CurrentTime > TimerLength)
+            CompleteTimer();
+    }
+
+    private void CompleteTimer()
+    {
+        TimerCompleteAction?.Invoke();
+        m_CurrentTime = 0;
+        IsActive = Loop;
+    }
+}
