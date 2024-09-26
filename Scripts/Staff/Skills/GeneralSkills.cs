67c57674de461522b00a9a65991ae17ccc9165c8
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-26 16:25:30 +1000

    Made skills a bit more dynamioc

--- Scripts/Staff/Skills/GeneralSkills.cs
+++ Scripts/Staff/Skills/GeneralSkills.cs
@@ -3,56 +3,90 @@ using Godot;
 
 namespace GameDevInc;
 
+public enum ESkillType
+{
+    Programming,
+    Artist2D,
+    Artist3D,
+    Marketing,
+    Actor, 
+    Production,
+    QA,
+    Writer,
+    Designer,
+    Sound,
+    General
+}
 
 public class GeneralSkills
 {
     public const int MAX_SKILL_POINTS = 25;                           // Max skill points any category can have
-    public int Speed { get; private set; }
-    public int Quality { get; private set; }
-    public int AttentionToDetail { get; private set; }
-    public int Creativity { get; private set; }
 
-    public bool AddSpeedPoints(int points = 1)
+    private List<Skill> _skills = new List<Skill>();
+
+    public GeneralSkills()
     {
-        if(Speed + points <= MAX_SKILL_POINTS)
+        var rand = new RandomNumberGenerator();
+        rand.Randomize();
+        _skills = new List<Skill>
         {
-            Speed += points;
-            return true;
-        }
-
-        return false;
+            new Skill("Speed", rand.RandiRange(1, MAX_SKILL_POINTS), ESkillType.General),
+            new Skill("Quality", rand.RandiRange(1, MAX_SKILL_POINTS), ESkillType.General),
+            new Skill("AttentionToDetails", rand.RandiRange(1, MAX_SKILL_POINTS), ESkillType.General),
+            new Skill("Creativity", rand.RandiRange(1, MAX_SKILL_POINTS), ESkillType.General)
+        };
     }
 
-    public bool AddQualityPoints(int points = 1)
-    { 
-        if(Quality + points <= MAX_SKILL_POINTS)
-        {
-            Quality += points;
-            return true;
-        }
-
-        return false;
+    public GeneralSkills(List<Skill> skills)
+    {
+        _skills = skills;
     }
 
-    public bool AddAttentionToDetailPoints(int points = 1)
+    /// <summary>
+    /// Finds the skill with a specific name
+    /// </summary>
+    /// <param name="skillName">Name of the skill</param>
+    /// <returns>Reference to the skill</returns>
+    public Skill GetSkill(string skillName)
     {
-        if(AttentionToDetail + points <= MAX_SKILL_POINTS)
+        foreach(var skill in _skills)
         {
-            AttentionToDetail += points;
-            return true;
+            if (skill.SkillName == skillName)
+                return skill;
         }
 
-        return false;
+        return null;
     }
 
-    public bool AddCreativity(int points = 1)
+    /// <summary>
+    /// Gets all the skills in a category
+    /// </summary>
+    /// <param name="type">Category</param>
+    /// <returns>List of skills</returns>
+    public List<Skill> GetAllSkillsInCategory(ESkillType type)
     {
-        if(Creativity + points <= MAX_SKILL_POINTS)
+        var skills = new List<Skill>();
+
+        foreach(var skill in _skills)
         {
-            Creativity += points;
-            return true;
+            if (skill.SkillType == type)
+                skills.Add(skill);
         }
 
-        return false;
+        return skills;
+    }
+}
+
+public class Skill
+{
+    public string SkillName { get; private set; }
+    public int SkillValue;
+    public ESkillType SkillType { get; private set; }
+
+    public Skill(string name, int value = 1, ESkillType type)
+    {
+        SkillName = name;
+        SkillValue = value;
+        SkillType = type;
     }
 }
