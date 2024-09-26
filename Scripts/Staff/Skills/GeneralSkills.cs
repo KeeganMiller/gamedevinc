26a17d43110fc2740646e0f08f647af52a9c4e93
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-26 15:07:16 +1000

    Created the general skills class

---
+++ Scripts/Staff/Skills/GeneralSkills.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GameDevInc;
+
+
+public class GeneralSkills
+{
+    public const int MAX_SKILL_POINTS = 25;                           // Max skill points any category can have
+    public int Speed { get; private set; }
+    public int Quality { get; private set; }
+    public int AttentionToDetail { get; private set; }
+
+    public bool AddSpeedPoints(int points = 1)
+    {
+        if(Speed + points <= MAX_SKILL_POINTS)
+        {
+            Speed += points;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool AddQualityPoints(int points = 1)
+    { 
+        if(Quality + points <= MAX_SKILL_POINTS)
+        {
+            Quality += points;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool AddAttentionToDetailPoints(int points = 1)
+    {
+        if(AttentionToDetail + points <= MAX_SKILL_POINTS)
+        {
+            AttentionToDetail += points;
+            return true;
+        }
+
+        return false;
+    }
+}
