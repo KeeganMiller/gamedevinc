e06d6922cdfd9adc58196b3036977f53568693ff
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-26 15:07:25 +1000

    Created the programmer skill class

---
+++ Scripts/Staff/Skills/ProgrammerSkills.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GameDevInc;
+
+public class ProgrammerSkills
+{
+    public int Mathematics { get; private set; }
+    public int Debugging { get; private set; }
+    public int Graphics { get; private set; }
+    public int AI { get; private set; }
+    public int Networking { get; private set; }
+
+    public ProgrammerSkills()
+    {
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();
+        Mathematics = rand.RandiRange(1, GeneralSkills.MAX_SKILL_POINTS);
+        Debugging = rand.RandiRange(1, GeneralSkills.MAX_SKILL_POINTS);
+        Graphics = rand.RandiRange(1, GeneralSkills.MAX_SKILL_POINTS);
+        AI = rand.RandiRange(1, GeneralSkills.MAX_SKILL_POINTS);
+        Networking = rand.RandiRange(1, GeneralSkills.MAX_SKILL_POINTS);
+    }
+
+    public c
+}
